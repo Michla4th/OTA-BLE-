95562ebe6a02226182ddba0590eb8808274623bd
Le kha <lka.lnguyen1707@gmail.com>
2025-07-14 11:38:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import ota
-# import upload
 import time
 
 ota_device = ota.OTA()
 
-# def main():
-#     try:
-#         upload.setup(ota_device)
-#         upload.loop()
-#     except Exception as e:
-#         print(f"error: {e}")
-
-def my_new_handler(data):
-    ota_device.send_data(data)
-    print("Dữ liệu mới:", data)
-
 def setup():
-    ota_device.set_fallback_handler(my_new_handler)
-    ota_device.send_data("Begin")
+    ota_device.send_data("Xin chào ESP32")
 
 def loop():
     while True:
-        # ota_device.send_data("Xin chào từ upload.py")
         time.sleep(10)
 
 if __name__ == "__main__":
